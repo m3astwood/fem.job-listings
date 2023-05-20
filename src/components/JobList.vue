c88678de911e7af564b8a059bd9eedbf69ff718a
suroh <mx@suroh.tk>
2023-05-20 11:57:34 +0200

    organised files added in styles

---
+++ src/components/JobList.vue
@@ -0,0 +1,6 @@
+<script setup>
+</script>
+
+<template>
+  <h1>job list</h1>
+</template>
---
+++ src/stores/jobList.js
@@ -0,0 +1,7 @@
+import { defineStore } from 'pinia';
+
+// API Endpoint
+// https://api.npoint.io/11c3acdb4b08e878fc21
+
+export const useJobListStore = defineStore('jobList', () => {
+});
